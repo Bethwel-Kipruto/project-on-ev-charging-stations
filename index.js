8a68b4547f3b429b6d4d0f6a954d6b45a6b99a02
BETHWEL <bethwelk85@gmail.com>
2023-04-06 10:34:28 +0300

    adding buttons

--- index.js
+++ index.js
@@ -34,21 +34,61 @@ function displayChargers(chargers){
     <h5 style="text-align:center">${chargers.description}</h5>
     <h5 style="text-align:center">${chargers.cost}</h5>
     <h5 style="text-align:center">${chargers.compatibility}</h5>
-    <div class="baton"><button class="book-now-btn" >BOOK NOW</button></div>
-    <p class="station-status">THIS STATION IS FREE</p>
+    <div class="baton"><button class="book-now-btn" >BOOK FROM 8am-10am</button></div>
+    <div class="baton"><button class="book-now-btn2" >BOOK FROM 10am-12am</button></div>
+    <div class="baton"><button class="book-now-btn3" >BOOK FROM 12am-2pm</button></div>
+    <div class="baton"><button class="book-now-btn4" >BOOK FROM 2pm-4pm</button></div>
   `;
   //
   document.querySelector("#displayChargers").appendChild(chargercards);
 
   const btn = chargercards.querySelector(".book-now-btn");
+  const btn1 = chargercards.querySelector(".book-now-btn2");
+  const btn2 = chargercards.querySelector(".book-now-btn3");
+  const btn3 = chargercards.querySelector(".book-now-btn4");
+
+
+
   const status = chargercards.querySelector(".station-status");
 
   btn.addEventListener("click", function() {
-    const date = prompt("Choose a date for booking the charging station (YYYY-MM-DD format):");
-    const timefrom = prompt("Choose a time from which you want to charge your car(am/pm format):");
-    const timeto = prompt("Choose a time to which you'd want to charge your car (the time you'll be done charging your car) (am/pm format):");
-    const names = prompt("Write your customer code here (if you don't have one contact our office):");
-    btn.innerText = "THIS STATION IS BOOKED";
+    const date = prompt("Choose a date for booking the charging station (DD-MM-YYYY format):");
+    const names = prompt("WRITE YOUR NAME HERE:");
+    btn.innerText = "THIS STATION IS BOOKED FROM 8am-10am";
     status.innerText = `THIS CHARGING STATION IS BOOKED BY #${names} ON ${date} FROM ${timefrom} TO ${timeto}`;
   });
+
+  btn1.addEventListener("click", function() {
+    const date = prompt("Choose a date for booking the charging station (DD-MM-YYYY format):");
+    const names = prompt("WRITE YOUR NAME HERE:");
+    btn1.innerText = "THIS STATION IS BOOKED FROM 10am-12am";
+    status.innerText = `THIS CHARGING STATION IS BOOKED BY #${names} ON ${date} FROM ${timefrom} TO ${timeto}`;
+  });
+
+  btn2.addEventListener("click", function() {
+    const date = prompt("Choose a date for booking the charging station (DD-MM-YYYY format):");
+    const names = prompt("WRITE YOUR NAME HERE:");
+    btn2.innerText = "THIS STATION IS BOOKED FROM 12am-2pm";
+    status.innerText = `THIS CHARGING STATION IS BOOKED BY #${names} ON ${date} FROM ${timefrom} TO ${timeto}`;
+  });
+  
+  btn3.addEventListener("click", function() {
+    const date = prompt("Choose a date for booking the charging station (DD-MM-YYYY format):");
+    const names = prompt("WRITE YOUR NAME HERE:");
+    btn3.innerText = "THIS STATION IS BOOKED FROM 2pm-4pm";
+    status.innerText = `THIS CHARGING STATION IS BOOKED BY #${names} ON ${date} FROM ${timefrom} TO ${timeto}`;
+  });
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
